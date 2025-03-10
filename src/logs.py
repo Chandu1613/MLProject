af23ee5a30654e979e87e5c3e6f0843f4785448e
chandu16 <jagadishdevu523@icloud.com>
2025-03-10 20:03:49 +0530

    update logs

--- src/logs.py
+++ src/logs.py
@@ -1,11 +1,7 @@
 import logging
-import os
-
-log_dir = os.path.join(os.path.dirname(__file__), "logs")
-os.makedirs(log_dir, exist_ok=True)
 
 logging.basicConfig(
-    filename=os.path.join(log_dir, "pipeline.log"),
+    filename="logs/pipeline.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
