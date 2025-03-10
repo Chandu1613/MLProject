7e8d039adf16c3fa0ff6adcc72b60e08e730b03a
chandu16 <jagadishdevu523@icloud.com>
2025-03-10 20:05:41 +0530

    add main.py file and testing the app

---
+++ main.py
@@ -0,0 +1,16 @@
+import os
+import sys
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "src")))
+
+from src.data_ingestion import load_data
+
+if __name__ == "__main__":
+    dataset_path = "datasets/winequality.zip"
+    
+    if not os.path.exists(dataset_path):
+        raise FileNotFoundError(f"Dataset file '{dataset_path}' not found!")
+
+    df = load_data(dataset_path)
+    print("Dataset loaded successfully! First few rows:")
+    
